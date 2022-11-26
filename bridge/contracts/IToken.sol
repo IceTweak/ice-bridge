a1becc1c06a6c9adc81b36ec18e15660f5ba4727
IceTweak <35110513+IceTweak@users.noreply.github.com>
2022-11-27 00:12:38 +0600

    Delete IToken.sol

--- bridge/contracts/IToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: ISC
-
-pragma solidity ^0.8.17;
-
-interface IToken {
-    function mint(address to, uint256 amount) external;
-
-    function burn(address owner, uint256 amount) external;
-}

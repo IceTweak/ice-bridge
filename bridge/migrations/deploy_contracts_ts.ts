96574d5a6f9b2dce60a1a877aa402f710f8ea63d
Роман Савощик <tweakice@gmail.com>
2023-01-11 18:30:24 +0600

    R_feat:fix - refactor deployment process

--- bridge/migrations/deploy_contracts_ts.ts
+++ bridge/migrations/deploy_contracts_ts.ts
@@ -6,25 +6,14 @@ module.exports = (artifacts: Truffle.Artifacts, web3: Web3) => {
     network: Network,
     accounts: string[]
   ) => {
+
+    /**@dev - Deploy to ganache (private) network */
     const TokenFactory = artifacts.require("TokenFactory");
     const SoloBridge = artifacts.require("SoloBridge");
     const DestToken = artifacts.require("DestToken");
 
-    // deployer.deploy(TokenFactory, "OriginToken", "OT");
-    // deployer.deploy(TokenFactory, "BridgingToken", "BT");
+    deployer.deploy(TokenFactory, "OriginToken", "OT");
     deployer.deploy(SoloBridge);
-
-    // const originToken = await TokenFactory.deployed();
-    // console.log(
-    //   `TokenFactory deployed at ${originToken.address} in network: ${network}.`
-    // );
-    // const bridgingToken = await TokenFactory.deployed();
-    // console.log(
-    //   `TokenFactory deployed at ${bridgingToken.address} in network: ${network}.`
-    // );
-    const soloBridge = await SoloBridge.deployed();
-    console.log(
-      `SoloBridge deployed at ${soloBridge.address} in network: ${network}.`
-    );
   };
 };
+/**@dev - Need deploy to BSC testnet */

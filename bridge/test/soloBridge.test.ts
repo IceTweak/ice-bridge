7377b251faec253f43ab138bae0560400000c118
Роман Савощик <tweakice@gmail.com>
2023-01-18 12:23:02 +0600

    R_feat - complete tests for SoloBridge

--- bridge/test/soloBridge.test.ts
+++ bridge/test/soloBridge.test.ts
@@ -45,17 +45,39 @@ describe("SoloBridge", () => {
       );
     });
 
-    xit("should mint from dest token", async () => {
-      await expect(soloBridge.createBridge(token.address)).to.emit(
-        soloBridge,
-        "BridgeCreated"
-      );
-      const destAddress = await soloBridge.allBridges(0);
-      let destToken = new ethers.Contract(destAddress, DestToken.abi, sender);
+    context(">> sending transactions", () => {
+      let destToken: Contract;
 
-      await destToken.connect(wallet).mint(receiver1.address, TEST_VALUE);
+      beforeEach(async () => {
+        await soloBridge.createBridge(token.address);
+        let destAddress = await soloBridge.allBridges(0);
+        destToken = new ethers.Contract(destAddress, DestToken.abi, sender);
 
-      expect(await destToken.balanceOf(receiver1.address)).to.eq(TEST_VALUE);
+        // Send tx by wallet address (onlyBridge modifier)
+        await destToken.connect(wallet).mint(receiver1.address, TEST_VALUE);
+      });
+
+      it("should mint from dest token", async () => {
+        expect(await destToken.balanceOf(receiver1.address)).to.eq(TEST_VALUE);
+      });
+
+      it("should burnFrom address", async () => {
+        // Approve spending funds from address for bridgeWallet
+        await destToken.connect(receiver1).approve(wallet.address, TEST_VALUE);
+        // Send tx after approving
+        await destToken.connect(wallet).burnFrom(receiver1.address, TEST_VALUE);
+        
+        expect(await destToken.balanceOf(receiver1.address)).to.eq(0);
+      });
+
+      it("should revert on sending transaction by non bridgeWallet", async () => {
+        await expect(
+          destToken.connect(receiver1).mint(receiver1.address, TEST_VALUE)
+        ).to.be.reverted;
+        await expect(
+          destToken.connect(receiver1).burnFrom(receiver1.address, TEST_VALUE)
+        ).to.be.reverted;
+      });
     });
   });
 });
